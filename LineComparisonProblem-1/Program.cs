c0715a939147d86ee9d9c16684dc1f9ff4c8ab08
venubabu <velpurivenubabu123@gmail.com>
2023-04-09 09:17:09 +0530

    [Venubabu] Adding Welcome message

---
+++ LineComparisonProblem-1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LineComparisonProblem_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program");
+        }
+    }
+}
